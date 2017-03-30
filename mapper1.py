8f7daeeb1315921ab59056c721759341ff0ce27d
zhanghan061 <hz2439@columbia.edu>
2017-03-30 10:23:39 +0800

    Add files via upload

---
+++ mapper1.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+#!/usr/bin/env python
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    words = line.split()
+    print '%s\t%s' % (words[7], 1)
+
